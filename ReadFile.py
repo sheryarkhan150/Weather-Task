9ca125e06137e082bb1ad17ab2293fae44d84380
Sheryar Khan <sheri.ali.khan@gmail.com>
2023-10-05 19:41:07 +0500

    created a function for reading the file

--- ReadFile.py
+++ ReadFile.py
@@ -1,5 +1,5 @@
 import csv
-def read_report(weather):
+def read_report(file_path):
 
     with open("weather_1.csv","r") as file:
         csvreader = csv.reader(file)
@@ -10,5 +10,10 @@ def read_report(weather):
             rows.append(row)
         print(rows)
         file.close()
-
+    data = []
+    with open(file_path,"r") as file:
+        csvreader = csv.reader(file)
+        for row in csvreader:
+            data.append(row)
+    return data
 weather = read_report("/home/sheryar/Documents/github/WeatherTask/weather_1.csv")
