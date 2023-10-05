57559e8c182d4af00fcb2c6f2b47e7d6d0e9d7df
Sheryar Khan <sheri.ali.khan@gmail.com>
2023-10-05 19:37:08 +0500

    ReadFile

--- ReadFile.py
+++ ReadFile.py
@@ -1,11 +1,14 @@
 import csv
+def read_report(weather):
 
-file = open("weather_1.csv")
-csvreader = csv.DictReader(file)
-header = next(csvreader)
-print(header)
-rows = []
-for row in csvreader:
-    rows.append(row)
-print(rows)
-file.close()
+    with open("weather_1.csv","r") as file:
+        csvreader = csv.reader(file)
+        header = next(csvreader)
+        print(header)
+        rows = []
+        for row in csvreader:
+            rows.append(row)
+        print(rows)
+        file.close()
+
+weather = read_report("/home/sheryar/Documents/github/WeatherTask/weather_1.csv")
